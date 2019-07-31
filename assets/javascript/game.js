eae27185b740c2c8c3ee8ce90f92f601bce7e6bc
Deya Genao <deyagenao@gmail.com>
2019-07-31 19:25:30 -0400

    added javascript to replace underscores with guessed letters in the current word

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,23 +8,42 @@ var winsText = document.getElementById("wins-text");
 var guessesText = document.getElementById("guesses-text");
 var lettersGuessed = document.getElementById("letters-guessed");
 var directionsText = document.getElementById("directions-text");
+var letter1 = document.getElementById("letter-1");
+var letter2 = document.getElementById("letter-2");
+var letter3 = document.getElementById("letter-3");
+var letter4 = document.getElementById("letter-4");
+var letter5 = document.getElementById("letter-5");
 
 // Round 1: Tiger 
 // Each round will be a set word. For each round:
 // Listen for user’s input. Listen for key pressed. Guess count decreases each time that a key is pressed, unless key has already been pressed 
 // Onkeyup:
-document.onkeyup = function(event) {
+document.onkeyup = function (event) {
     // Determines which key was pressed 
     var userGuess = event.key;
     // Hide directions 
-    directionsText.textContent = "";
-// Use the .toLowercase method.
-//  if letter is in the word, underscore in word will be replaced with letter
+    directionsText.textContent = "Round 1";
+    // Use the .toLowercase method.
+    //  if letter is in the word, underscore in word will be replaced with letter
     if (userGuess === "t" || userGuess === "i" || userGuess === "g" || userGuess === "e" || userGuess === "r") {
-
-    }
-// Maybe use something like .charAt() to fill in letters at specific spaces, like  in banana, b will fill in .charAt(0)--> check if this index works the same way as in an array
-//for (i = 0; i < zooArray.length; i++) {
+        //for each letter, replace a specific underscore 
+        if (userGuess === "t") {
+            letter1.textContent = "T";
+        }
+        else if (userGuess === "i") {
+            letter2.textContent = "I";
+        }
+        else if (userGuess === "g") {
+            letter3.textContent = "G";
+        }
+        else if (userGuess === "e") {
+            letter4.textContent = "E";
+        }
+        else {
+            letter5.textContent = "R";
+        }
+        // Maybe use something like .charAt() to fill in letters at specific spaces, like  in banana, b will fill in .charAt(0)--> check if this index works the same way as in an array
+        //for (i = 0; i < zooArray.length; i++) {
         // check if elemnt starts with c or o
         // if (zooArray[i].charAt(0) === "c" || zooArray[i].charAt(0) === "o") {
         //     alert("starts with c or o");
@@ -32,12 +51,13 @@ document.onkeyup = function(event) {
         //     alert("no it doesnt, for zooArray at " + i )
         //   }
         // }
-// Else if letter is not in word: 
+    }
+    // Else if letter is not in word: 
     else {
         //letter will be added to text below 
         // create new element, include userGuess and then append the child? 
         lettersGuessed.textContent = userGuess;
-        
+
 
     }
     //Guess count goes down by one
