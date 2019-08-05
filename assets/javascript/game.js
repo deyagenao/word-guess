3d303a7bd1015536f80570352056bfa1d055a3eb
Deya Genao <deyagenao@gmail.com>
2019-08-04 21:06:18 -0400

    made formatting changes to text and added javascript to know when guessed word is completed and when guesses run out

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-//Variables to keep track of correct letters guessed, wins and total guesses
+//Variables to keep track of wins, guesses and letters guessed (corrent and incorrect)
 var wins = 0;
 var guesses = 12;
 // potential issue with this array, what if the same letter appears more than once in the word?
@@ -8,7 +8,7 @@ var wrongLettersGuessed = [];
 // Set up variables to display on page
 var winsText = document.getElementById("wins-text");
 var guessesText = document.getElementById("guesses-text");
-var wrongLettersGuessedElement = document.getElementById("wrong-letters-guessed");
+var wrongLettersGuessedText = document.getElementById("wrong-letters-guessed");
 var directionsText = document.getElementById("directions-text");
 var currentWordText = document.getElementById("current-word-text");
 
@@ -26,7 +26,6 @@ var animalWordsToGuess = [
   "zebra",
   "blobfish"
 ];
-// var currentWord = "";
 
 //function for choosing a random word from the array, maybe doesn't even have to be a function..? 
 // function chooseRandomAnimalWord() {
@@ -40,21 +39,21 @@ console.log(chosenWord);
 var hiddenWord = [];
 var hideChosenWord = function () {
     for (var i = 0; i < chosenWord.length; i++) {
-        hiddenWord.push('_ ');
+        hiddenWord.push('_');
     }
     return hiddenWord;
 }
 console.log(hideChosenWord());
+console.log(hiddenWord);
 //convert the hidden word array to a string to display on page-- not working because commas appear between the underscores
-// var currentWord = hiddenWord.toString();
+var currentWord = hiddenWord.toString();
+currentWordText.textContent = currentWord.replace(/,/g, " ");
+
 // console.log(currentWord);
 
 //check if guess is right 
 //if guess is right, change letter in underscores 
 //if guess is wrong, add the letter to the guessed letters div 
-
-//use property of the variable word to be able to identify when the word is completed, also create a variable to keep track of how many correct letters guessed, and equal them?:
-// if (correctLettersGuessed == word.length)
 
 // Listen for user’s input. Listen for key pressed. Guess count decreases each time that a key is pressed, unless key has already been pressed
 // Onkeyup:
@@ -64,21 +63,41 @@ document.onkeyup = function(event) {
     // Determines which key was pressed
     var userGuess = event.key;
     // Hide directions
-    directionsText.style.display = "none";
-    //check if letter is in the chosen word 
-    if (chosenWord.indexOf(userGuess) > -1) {
-        //letter is added to correct guesses 
-        correctLettersGuessed.push(userGuess);
-    } 
-    // if letter is not in the chosen word:  
-    else {
-        //letter is added to incorrect guesses
-        wrongLettersGuessed.push(" " + userGuess);
-        var wrongLettersGuessedText = wrongLettersGuessed.toString();
-        wrongLettersGuessedElement.textContent = wrongLettersGuessedText;
-    }
+    directionsText.textContent = "Guess, until you can't guess anymore!";
+    // check that the user guess is a letter and that it has not already been guessed
+    if (letters.indexOf(userGuess) > -1 && correctLettersGuessed.indexOf(userGuess) == -1 && wrongLettersGuessed.indexOf(" " + userGuess) == -1) {
+            //check if letter is in the chosen word 
+        if (chosenWord.indexOf(userGuess) > -1) {
+            //letter is added to correct guesses 
+            correctLettersGuessed.push(userGuess);
+            //replace underscore from hidden word with the correct letter
+            hiddenWord[chosenWord.indexOf(userGuess)] = userGuess;
+        } 
+        // if letter is not in the chosen word:  
+        else {
+            //letter is added to incorrect guesses
+            wrongLettersGuessed.push(" " + userGuess);
+            wrongLettersGuessedText.textContent = wrongLettersGuessed.toString();
+        }
+        //Guess count goes down by one
+        guesses--;
+        //Display new number of guesses
+        guessesText.textContent = guesses;
+    };
+    //if all underscores have been filled, then player wins the game and game restarts 
+    if (hiddenWord.indexOf("_") === -1) {
+        directionsText.textContent = "You win! You should totally play again.";
+        wins++;
+        winsText.textContent = wins;
+    };
+    //if number of guesses reaches 0 and there is at least one letter missing in the word, the player loses and the game restarts 
+    if (guesses === 0 && hiddenWord.indexOf("_") > -1) {
+        directionsText.textContent = "You lose! Maybe you could try again...";
+    };
+   
     console.log(correctLettersGuessed);
     console.log(wrongLettersGuessed);
+    console.log(hiddenWord);
   
   //  if letter is in the word, underscore in word will be replaced with letter   
 
@@ -86,14 +105,12 @@ document.onkeyup = function(event) {
 //     // create new element, include userGuess and then append the child?
 //     wrongLettersGuessedText.textContent = userGuess;
 //   }
-  //Guess count goes down by one
-  guesses--;
-  //Display new number of guesses
-  guessesText.textContent = guesses;
 };
 // Else: nothing happens
 // When all underscores have been replaced, image/ sound will be revealed. Wins number +1
 //If user does not guess the word in under 12 guesses, game starts over?
+//use property of the variable word to be able to identify when the word is completed, also create a variable to keep track of how many correct letters guessed, and equal them?:
+// if (correctLettersGuessed == word.length)
 // Next round button appears
 
 // When user clicks the button, new underscores appear, text is cleared from letters guessed
